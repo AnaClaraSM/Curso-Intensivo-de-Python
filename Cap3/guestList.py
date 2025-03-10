f02674458ee8f639ddcab0335425bb7f0248e540
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2025-03-10 17:00:23 -0300

    exercise 3.7

--- Cap3/guestList.py
+++ Cap3/guestList.py
@@ -1,5 +1,8 @@
 # EXERCÍCIO 3.4: Crie uma lista de convidados para um jantar e exiba uma mensagem de convite a cada pessoa.
 
+print(f"É com prazer que queremos anunciar a organização de um jantar de comemoração aos 10 anos do setor 9 de TI em nossa empresa. Conseguimos uma mesa com lugar para 6 dos nossos queridos funcionários. Os convidados serão notificados em breve.")
+print()
+
 # Lista de convidados
 convidados = ["Will", "Louise", "Carl", "Amelia", "Madson", "Oliver"]
 
@@ -69,5 +72,66 @@ del convidados[-1] #Por índice
 print(f"Olá, {convidados[-1]}! {mensagem_convite}")
 
 print()
+
 # Exibe lista de convidados
 print(f"Convidados: {convidados}")
+print()
+
+
+# EXERCÍCIO 3.7: Remova os convidados da lista até sobrarem dois, envie mensagens, depois esvazie a lista
+
+# Exibe aviso de redução de lugares
+print(f"Lamentamos muito em informar que nossa mesa não chegará a tempo, e agora poderemos chamar somente dois dos convidados previstos. Pedimos sinceras desculpas a todos, em especial aos que não poderão comparecer.")
+
+# Exibe lista inicial
+print(f"Lista Incial: {convidados}") # 9 convidados
+
+# Remoção dos convidados e aviso
+
+convidado_removido = convidados.pop() #Restam 8
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+convidado_removido = convidados.pop() #Restam 7
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+convidado_removido = convidados.pop() #Restam 6
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+convidado_removido = convidados.pop() #Restam 5
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+convidado_removido = convidados.pop() #Restam 4
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+convidado_removido = convidados.pop() #Restam 3
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+convidado_removido = convidados.pop() #Restam 2
+print(f"Olá {convidado_removido}. É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade.")
+print()
+
+# Aviso aos convidados restantes
+
+print(f"Olá {convidados[0]}! Gostaríamos de informar que você permanece em nossa lista de convidados! Esperamos que possa aproveitar o jantar ao máximo.")
+
+print(f"Olá {convidados[1]}! Gostaríamos de informar que você permanece em nossa lista de convidados! Esperamos que possa aproveitar o jantar ao máximo.")
+
+
+# Exibe lista final
+print(f"Lista Final: {convidados}") # 2 convidados
+
+
+# Esvazia a lista -> convidados.clear() ou = [] são possibilidades
+# del convidados[0] 
+# del convidados[1] #Erro -> ao remover o 0 primeiro, o elemento de posição 1 é deslocado para a posição 0, eliminando o índice 1
+del convidados[1]
+del convidados[0] 
+
+# Exibe lista esvaziada
+print(f"Lista de Convidados: {convidados}")
--- Cap3/guestList_v2.py
+++ Cap3/guestList_v2.py
@@ -1,5 +1,8 @@
 # EXERCÍCIO 3.4: Crie uma lista de convidados para um jantar e exiba uma mensagem de convite a cada pessoa.
 
+print(f"É com prazer que queremos anunciar a organização de um jantar de comemoração aos 10 anos do setor 9 de TI em nossa empresa. Conseguimos uma mesa com lugar para 6 dos nossos queridos funcionários. Os convidados serão notificados em breve.")
+print()
+
 # Lista de convidados
 convidados = ["Will", "Louise", "Carl", "Amelia", "Madson", "Oliver"]
 
@@ -8,7 +11,7 @@ mensagem_convite = "Estamos organizando um jantar e gostaríamos de contar com a
 
 # Exibição da mensagem para cada convidado da lista de convidados
 for convidado in convidados:
-    print(f"Olá, {convidado}! {mensagem_convite}")
+    print(f"\tOlá, {convidado}! {mensagem_convite}")
 print()
 
 
@@ -26,7 +29,7 @@ convidados.insert(2, "Sophie")
 # Aviso de ausência
 print(f"Lamentamos informar que o convidado {convidado_removido} não poderá comparecer ao jantar.") #como alternar o sufixo dependendo do gênero?
 
-print(f"Olá {convidados[2]}! {mensagem_convite}")
+print(f"\tOlá {convidados[2]}! {mensagem_convite}")
 print()
 
 
@@ -38,11 +41,11 @@ print("Gostariamos de informar, alegremente, que encontramos uma mesa maior para
 # Adiciona convidado no início da lista
 convidados.insert(0, "Sunny")
 # Envia mensagem ao convidado
-print(f"Olá, {convidados[0]}! {mensagem_convite}")
+print(f"\tOlá, {convidados[0]}! {mensagem_convite}")
 
 # Adiciona convidado no meio da lista
 convidados.insert(3, "Garret")
-print(f"Olá, {convidados[3]}! {mensagem_convite}")
+print(f"\tOlá, {convidados[3]}! {mensagem_convite}")
 
 # Adiciona convidado no final da lista (com repetições "indevidas")
 convidados.append("Harold")
@@ -53,9 +56,43 @@ convidados.append("Harold")
 convidados.remove("Harold") #Por nome
 del convidados[-1] #Por índice
 
-print(f"Olá, {convidados[-1]}! {mensagem_convite}")
+print(f"\tOlá, {convidados[-1]}! {mensagem_convite}")
 
 print()
 
-# Exibe lista final de convidados
-print(f"Convidados: {convidados}")
+
+# EXERCÍCIO 3.7: Remova os convidados da lista até sobrarem dois, envie mensagens, depois esvazie a lista
+
+# Exibe aviso de redução de lugares
+print(f"Lamentamos muito em informar que nossa mesa não chegará a tempo, e agora poderemos chamar somente dois dos convidados previstos. Pedimos sinceras desculpas a todos, em especial aos que não poderão comparecer.")
+
+
+# Remoção dos convidados e aviso de desconvite
+
+mensagem_desconvite = "É com tristeza que informamos que não poderemos mais ter-lhe em nosso jantar. Pedimos desculpas e esperamos poder compensar em uma próxima oportunidade."
+
+# Não percorra uma lista com for enquanto remove elementos dela dentro do for -> erros
+
+# Enquanto houver mais de dois convidados
+while len(convidados) > 2:
+    # Remove convidado do final e armazena
+    convidado_removido = convidados.pop()
+    # Informa desconvite
+    print(f"\tOlá {convidado_removido}. {mensagem_desconvite}")
+print()
+
+# Aviso aos convidados restantes
+
+mensagem_permanencia = "Gostaríamos de informar que você permanece em nossa lista de convidados! Esperamos que possa aproveitar o jantar ao máximo."
+
+# Para cada convidado da lista
+for convidado in convidados:
+    # Informa a permanência
+    print(f"\tOlá {convidado}! {mensagem_permanencia}")
+    # Erro de retorno de mensagens apenas para Sunny -> Loop for com convidado[0] ao invés de convidado
+print()
+
+# Esvazia a lista
+convidados.clear() #clear() -> None
+
+print(f"Lista de Convidados: {convidados}")
